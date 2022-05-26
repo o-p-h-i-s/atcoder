3f907125d9ba2ea44f3e1489f7460f107d9ef4a5
o-p-h-i-s <0023dcd3b2@gmail.com>
2022-05-26 23:04:06 +0900

    Add

---
+++ B_Evilator.rs
@@ -0,0 +1,79 @@
+use proconio::input;
+
+fn main() {
+   input! {
+      s: String,
+   }
+   let s = s.chars().collect::<Vec<char>>();
+   let mut ans = (s.len() - 1) * 2;
+   for i in 1..s.len() - 1 {
+      ans += s.len() - 1;
+      if s[i] == 'U' {
+         ans += i;
+      } else {
+         ans += s.len() - i - 1;
+      }
+   }
+   println!("{}", ans);
+}
+
+#[allow(dead_code)]
+fn gcd(a: i64, b: i64) -> i64 {
+   if 0 < a {
+      return gcd(b % a, a);
+   } else {
+      return b;
+   }
+}
+
+#[allow(dead_code)]
+fn lcm(a: i64, b: i64) -> i64 {
+   return a / gcd(a, b) * b;
+}
+
+trait RunLengthEncoding {
+   fn rle(&self) -> Vec<(char, i64)>;
+}
+
+impl RunLengthEncoding for String {
+   fn rle(&self) -> Vec<(char, i64)> {
+      let s = self.chars().collect::<Vec<char>>();
+      let mut res = Vec::new();
+      let mut pre = ' ';
+      let mut count = 0;
+      for c in s.into_iter() {
+         if c == pre {
+            count += 1;
+         } else {
+            if pre != ' ' {
+               res.push((pre, count));
+            }
+            pre = c;
+            count = 1;
+         }
+      }
+      res.push((pre, count));
+      res
+   }
+}
+
+impl RunLengthEncoding for Vec<char> {
+   fn rle(&self) -> Vec<(char, i64)> {
+      let mut res = Vec::new();
+      let mut pre = ' ';
+      let mut count = 0;
+      for &c in self.into_iter() {
+         if c == pre {
+            count += 1;
+         } else {
+            if pre != ' ' {
+               res.push((pre, count));
+            }
+            pre = c;
+            count = 1;
+         }
+      }
+      res.push((pre, count));
+      res
+   }
+}
